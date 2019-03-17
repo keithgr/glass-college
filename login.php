9ab49ebdfcf594f96b1615e9e14f861522d725f1
Yuwei Chen <39499538+scygen@users.noreply.github.com>
2019-03-17 04:51:15 -0400

    Add files via upload
    html and php files

--- login.php
+++ login.php
@@ -1,3 +1,6 @@
+<?php
+
+$content = '
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -27,45 +30,55 @@
 	<link rel="stylesheet" type="text/css" href="css/main.css">
 <!--===============================================================================================-->
 </head>
-    <body>
-        <div class="limiter">
-            <div class="container-login100">
-            <div class="wrap-login100">
-        <form class="login100-form validate-form">
-            <span class="login100-form-title p-b-26">
-                Welcome
-            </span>
-            <form action = "insert_user.php" method = "post">
-        <div class="form-group">
-            <label for="name">UserName</label>
-            <input type="text" class="form-control" id="year" name="name">
-        </div>
-        <div class="form-group">
-            <label for="password">Password</label>
-            <input type="password" class="form-control" id="sem" name="password">
-        </div>
-    </form>
-            <div class="container-login100-form-btn">
-                <div class="wrap-login100-form-btn">
-                    <div class="login100-form-bgbtn"></div>
-                    <button class="login100-form-btn">
-                        <input type="submit">
-                    </button>
-                </div>
-            </div>
-            <div class="text-center p-t-115">
-                <span class="txt1">
-                    Don’t have an account?
-                </span>
-                <a class="txt2" href="register.html">
-                    Sign Up
-                </a>
-            </div>
-                </form>
-                </div>
-            </div>
-        </div>
-<div id="dropDownSelect1"></div>
+<body>
+	
+	<div class="limiter">
+		<div class="container-login100">
+			<div class="wrap-login100">
+				<form class="login100-form validate-form">
+					<span class="login100-form-title p-b-26">
+						Welcome
+					</span>
+					<span class="login100-form-title p-b-48">
+						<img src="img/eyeGlass.png" alt="sm_logo">
+					</span>
+
+					<div class="wrap-input100 validate-input" data-validate = "Valid email is: a@b.c">
+						<input class="input100" type="text" name="email">
+						<span class="focus-input100" data-placeholder="Email"></span>
+					</div>
+
+					<div class="wrap-input100 validate-input" data-validate="Enter password">
+						<span class="btn-show-pass">
+							<i class="zmdi zmdi-eye"></i>
+						</span>
+						<input class="input100" type="password" name="pass">
+						<span class="focus-input100" data-placeholder="Password"></span>
+					</div>
+
+					<div class="container-login100-form-btn">
+						<div class="wrap-login100-form-btn">
+							<div class="login100-form-bgbtn"></div>
+							<button class="login100-form-btn">
+								Login
+							</button>
+						</div>
+					</div>
+
+					<div class="text-center p-t-115">
+						<span class="txt1">
+							Don’t have an account?
+						</span>
+
+						<a class="txt2" href="index.html">
+							Sign Up
+						</a>
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
+	<div id="dropDownSelect1"></div>
 	
 <!--===============================================================================================-->
 	<script src="vendor/jquery/jquery-3.2.1.min.js"></script>
@@ -85,4 +98,9 @@
 	<script src="js/main2.js"></script>
 
 </body>
-</html>
+</html>';
+
+session_start();
+
+
+?>
