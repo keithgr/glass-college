e356703b1409e67a6c340209922508584f505ff2
Keith Grable <kw28grab@siena.edu>
2019-03-17 10:02:07 -0400

    Uploading files to server

--- upload.php
+++ upload.php
@@ -1,8 +1,8 @@
 <?php
     $target_dir = "/opt/lampp/uploads";
-    $target_file = $target_dir . basename($_FILES["vid"]["name"]);
-    $uploadOk = 1;
-    $fileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+    $target_file = $target_dir.basename($_FILES["vid"]["name"]);
 
-    move_uploaded_file($_FILES["vid"]["tmp_name"], $target_file);
+    echo $target_file;
+    
+    //move_uploaded_file($_FILES["vid"]["tmp_name"], $target_file);
 ?>
