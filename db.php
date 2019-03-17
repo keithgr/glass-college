b9a5256f296e2903b0bd6e3416afdf3cabd791f6
Keith Grable <kw28grab@siena.edu>
2019-03-16 23:20:32 -0400

    Putting back port

--- db.php
+++ db.php
@@ -7,12 +7,13 @@
         $config = parse_ini_file("/opt/lampp/config.ini");
 
         $host = $config['HOST'];
+        $port = $config['PORT'];
         $user = $config['USER'];
         $pass = $config['PASS'];
         $db = $config['DB'];
 
         // Create and verify connection
-        $sql = new mysqli($host, $user, $pass, $db);
+        $sql = new mysqli($host.':'.$port, $user, $pass, $db);
         if ($sql->connect_errno) {
            die("Failed to connect to MySQL: ($sql->connect_errno) $sql->connect_error");
         }
