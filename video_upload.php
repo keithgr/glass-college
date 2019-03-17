d62a17be23f48f11564541ab2452b2d363da833d
Keith Grable <kw28grab@siena.edu>
2019-03-17 08:46:40 -0400

    Creating video upload script

--- video_upload.php
+++ video_upload.php
@@ -37,23 +37,19 @@ $content = '
 			<div class="wrap-login100">
 				<form class="login100-form validate-form">
 					<span class="login100-form-title p-b-26">
-						Welcome
+						Share your experience!
 					</span>
+                    
 					<span class="login100-form-title p-b-48">
 						<img src="img/eyeGlass.png" alt="sm_logo">
 					</span>
 
-					<div class="wrap-input100 validate-input" data-validate = "Valid email is: a@b.c">
-						<input class="input100" type="text" name="email">
-						<span class="focus-input100" data-placeholder="Email"></span>
-					</div>
-
-					<div class="wrap-input100 validate-input" data-validate="Enter password">
+					<div class="wrap-input100 validate-input" data-validate="Must enter video title">
 						<span class="btn-show-pass">
 							<i class="zmdi zmdi-eye"></i>
 						</span>
-						<input class="input100" type="password" name="pass">
-						<span class="focus-input100" data-placeholder="Password"></span>
+						<input class="input100" type="text" name="title">
+						<span class="focus-input100" data-placeholder="Awesome college experience"></span>
 					</div>
 
 					<div class="container-login100-form-btn">
