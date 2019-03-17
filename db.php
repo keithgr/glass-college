018c1eb7318894bc7c2d7f5a3937e96f22609b0a
Keith Grable <kw28grab@siena.edu>
2019-03-16 22:35:50 -0400

    Adding database connectivity

---
+++ db.php
@@ -0,0 +1,24 @@
+<?
+    // Init config data
+    $config = parse_ini_file("/opt/lampp/config.ini");
+
+    $host = $config['HOST'];
+    $port = $config['PORT'];
+    $user = $config['USER'];
+    $pass = $config['PASS'];
+    $db = $config['DB'];
+
+
+
+    // Create and verify connection
+    $conn = mysql_connect($host.':'.port, $user, $pass, $db);
+    if(!$conn) {
+        die('Could not connect: '.mysql_error());
+    }
+    echo 'Connected successfully';
+
+
+
+    // Close connection
+    mysql_close($conn);
+?>
