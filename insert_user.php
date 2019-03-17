60fdf4bcf348b032d4183ff8697dff51902c3db6
Keith Grable <kw28grab@siena.edu>
2019-03-17 01:54:41 -0400

    More stuff

--- insert_user.php
+++ insert_user.php
@@ -8,27 +8,29 @@
 
         if($name && $password){
             $db = db_connect();
-            $sql = "INSERT INTO user ('name', 'pass') VALUES ('$name','$password')";
+            $sql = "INSERT INTO user ('name', 'pass', 'major_id') VALUES ('$name', '$password', NULL)";
             $output = $db->query($sql);
             $db->close();
         }
     }
 
-    $content = '<form action = "insert_user.php" method = "post">
-	<div class="form-group">
-		<label for="name">UserName</label>
-		<input type="text" class="form-control" id="year" name="name">
-	</div>
-	<div class="form-group">
-		<label for="password">Password</label>
-		<input type="text" class="form-control" id="sem" name="password">
-	</div>
-	<input type="submit">
-</form>';
+    $content = '
+    <form action = "insert_user.php" method = "post">
+        <div class="form-group">
+            <label for="name">UserName</label>
+            <input type="text" class="form-control" id="year" name="name">
+        </div>
+        <div class="form-group">
+            <label for="password">Password</label>
+            <input type="password" class="form-control" id="sem" name="password">
+        </div>
+        <input type="submit">
+    </form>';
 
 	$db = db_connect();
 	$sql = "SELECT * FROM user";
 	$result = $db->query($sql);
+	var_dump($result);
 	$content.= '<table class = "table">';
 	while($row = $result->fetch_assoc()){
 		$content.= '<tr>';
