51b2a8af1200f20f513ecaa1741e7bd262b51dad
Keith Grable <kw28grab@siena.edu>
2019-03-17 10:12:52 -0400

    Uploading files to server

--- upload.php
+++ upload.php
@@ -1,10 +1,11 @@
 <?php
     
+/*
     if($_SERVER['REQUEST_METHOD'] === 'POST') {
         $db = db_connect();
         $sql = "INSERT INTO video (username, college_id, name, description) VALUES ('{$_SESSION['user']}', '{$_SESSION['college_id']}', '{$_POST['title']}', '{$_POST['desc']}')";  
     }
-
+*/
     $target_dir = "/opt/lampp/uploads/";
     $target_file = $target_dir.basename($_FILES["vid"]["name"]);
     $uploadOk = 1;
