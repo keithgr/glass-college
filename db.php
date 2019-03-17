f32bbb9713570bd19f3170824ca0aec4485dc435
Keith Grable <kw28grab@siena.edu>
2019-03-16 22:51:50 -0400

    Removing port

--- db.php
+++ db.php
@@ -3,15 +3,14 @@
     $config = parse_ini_file("/opt/lampp/config.ini");
 
     $host = $config['HOST'];
-    $port = $config['PORT'];
     $user = $config['USER'];
     $pass = $config['PASS'];
     $db = $config['DB'];
 
-
+    
 
     // Create and verify connection
-    $conn = mysql_connect($host.':'.$port, $user, $pass, $db);
+    $conn = mysql_connect($host, $user, $pass, $db);
     if(!$conn) {
         die('Could not connect: '.mysql_error());
     }
