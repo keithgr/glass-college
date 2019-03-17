cf090ac5b9b02be52666e26555589850443aef3a
Keith Grable <kw28grab@siena.edu>
2019-03-17 06:39:24 -0400

    Makin money moves

--- db.php
+++ db.php
@@ -1,16 +1,9 @@
 <?
     require('functions.php');
 
-    $pn = $_GET['page_num'];
+    //$pn = $_GET['page_num'];
 
-    $result = get_colleges('technical institute', $pn);
-	$content .= '<table class = "table">';
-	while($row = $result->fetch_assoc()){
-		$content.= '<tr>';
-		foreach($row as $value){
-			$content.= '<td>'.$value.'</td>';
-		}
-		$content.= '</tr>';
-	}
+    //$result = get_colleges('technical institute', $pn);
+
     echo get_page_count('technical institute');
 ?>
